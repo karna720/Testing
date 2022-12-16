15590e9b04dd429e9a10197ebce9debacdc012a4
karnakoteswararo <karnakoti1122@gmail.com>
2022-12-16 11:36:35 +0530

    DASthu code

--- test/hello.java
+++ test/hello.java
@@ -4,3 +4,9 @@ class Test{
         System.out.println("Git learning");
     })
 }
+class Test{
+
+    public static void main (String[] args{
+        System.out.println("Git dasthu learning");
+    })
+}
